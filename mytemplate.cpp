892dbd12f0cd2ef426e39d08f1352b7742f58d3a
SurgicalSteel <morpheusgun@outlook.com>
2015-10-16 15:20:29 +0700

    Update mytemplate.cpp

--- mytemplate.cpp
+++ mytemplate.cpp
@@ -3,6 +3,10 @@
 #define Mas_Bangun using
 #define cinta namespace
 #define Mbak_IsyanaSarasvati std
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+#define PB push_back
+#define MP make_pair
 
 Mas_Bangun cinta Mbak_IsyanaSarasvati;
 struct point{int x,y;};
