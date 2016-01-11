1a1c626fb8dede87048c3f8967e3c3c662d407d3
Yuwono Bangun Nagoro <morpheusgun@outlook.com>
2016-01-11 13:28:46 +0700

    update

--- My CSharp Template for Programming Contest.cs
+++ My CSharp Template for Programming Contest.cs
@@ -3,6 +3,25 @@ using System.Text;
 using System.Collections;
 using System.Linq;
 using System.Collections.Generic;
+using System.Math;
+/*
+	Leave the sleep and let the springtime talk
+	In tongues from the time before man
+	Listen to a daffodil tell her tale
+	Let the guest in, walk out, be the first to greet the morn
+	The meadows of heaven await harvest
+	The cliffs unjumped, cold waters untouched
+	The elsewhere creatures yet unseen
+	Finally your number came up, free fall awaits the brave
+	Come
+	Taste the wine, race the blind
+	They will guide you from the light
+	Writing noughts till the end of time
+	Come
+	Surf the clouds, race the dark
+	It feeds from the runs undone
+	Meet me where the cliff greets the sea
+*/
 namespace myCSharpTemplate
 {
 	class MainClass
@@ -100,7 +119,7 @@ namespace myCSharpTemplate
 		{
 			//string tmp;
 			//int tc;
-			//tmp = Console.ReadLine ();
+			//tmp = int.Parse(Console.ReadLine ());
 			//int.TryParse (tmp, out tc);
 			Console.WriteLine("Hello World!");
 
