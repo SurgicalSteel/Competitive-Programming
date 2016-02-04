84896ab94d4c5a271b3faeb99995f238bc500c24
Yuwono Bangun Nagoro <morpheusgun@outlook.com>
2016-02-04 09:06:13 +0700

    Create Maximizing XOR.cpp

---
+++ Hackerrank/Maximizing XOR.cpp
@@ -0,0 +1,46 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <cstdlib>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+/*
+ * Complete the function below.
+ */
+int maxXor(int l, int r) {
+    int maxi=-100;
+    for(int i=l;i<=r;i++)
+    {
+        for(int j=l;j<=r;j++)
+        {
+            if((i^j)>maxi){maxi=i^j;}
+        }
+    }
+    return maxi;
+}
+
+int main() {
+    int res;
+    int _l;
+    cin >> _l;
+    
+    int _r;
+    cin >> _r;
+    
+    res = maxXor(_l, _r);
+    cout << res;
+    
+    return 0;
+}
