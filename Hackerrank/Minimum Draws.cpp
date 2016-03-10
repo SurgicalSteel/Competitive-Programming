4fc9e8f2aa545ad371a0dee40bb0d20bc55a9649
Yuwono Bangun Nagoro <morpheusgun@outlook.com>
2016-03-11 04:42:08 +0700

    Create Minimum Draws.cpp

---
+++ Hackerrank/Minimum Draws.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int tc,temp;
+    scanf("%d",&tc);
+    for(int i=0;i<tc;i++)
+    {scanf("%d",&temp);printf("%d\n",temp+1);}
+    return 0;
+}
