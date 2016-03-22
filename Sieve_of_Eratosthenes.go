aa6ed72e90a4016b81a2c072085f09f9a2fdeae2
Yuwono Bangun Nagoro <morpheusgun@outlook.com>
2016-03-22 10:32:42 +0700

    Create Sieve_of_Eratosthenes.go

---
+++ Sieve_of_Eratosthenes.go
@@ -0,0 +1,33 @@
+package main
+import "fmt"
+
+func main(){
+	var sz int=100000
+	var arr [99999]int
+	for x:=0;x<sz-1;x++{
+		arr[x]=x+2
+	}
+	var check int=0
+	for check<sz-1{
+		var iter int =0
+		for iter<sz-1{
+			if arr[iter]!=-1{
+				check++
+				var it int=iter
+				for it<sz-1{
+					if it!=iter&&arr[it]%arr[iter]==0&&arr[it]!=-1{
+						arr[it]=-1
+						check++
+					}
+					it+=arr[iter]
+				}
+			}
+			iter++
+		}
+	}
+	for x:=0;x<sz-1;x++{
+		if arr[x]!=-1{
+			fmt.Println(arr[x])
+		}
+	}
+}
