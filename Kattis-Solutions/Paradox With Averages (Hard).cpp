221a1ebc70bd4b4c0adf39d2bb849b3fbf30c8d8
Yuwono Bangun Nagoro <morpheusgun@outlook.com>
2016-04-01 23:45:22 +0700

    Create Paradox With Averages (Hard).cpp

---
+++ Kattis-Solutions/Paradox With Averages (Hard).cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#define psb push_back
+using namespace std;
+int main() {
+	int tc,ncs,nec;
+	float temp;
+	long double totcs,totec;
+	scanf("%d",&tc);
+	while(tc--){
+		totcs=0;
+		totec=0;
+		scanf("%d %d",&ncs,&nec);
+		long double avcs, avec;
+		vector<float> vcs,vec;
+		for(int i=0;i<ncs;++i)
+		{scanf("%f",&temp);vcs.psb(temp);totcs+=temp;}
+		avcs=totcs/ncs;
+		//printf("%.2f\n",avcs);
+		for(int i=0;i<nec;++i)
+		{scanf("%f",&temp);vec.psb(temp);totec+=temp;}
+		avec=totec/nec;
+		//printf("%.2f\n",avec);
+		sort(vcs.begin(),vcs.end());
+		int counter=0;
+		int x=0;
+		while(x<ncs)
+		{
+			long double avrescs=(totcs-vcs[x])/(ncs-1);
+			//printf("avrescs %.2f\n",avrescs);
+			long double avresec=(totec+vcs[x])/(nec+1);
+			//printf("avresec %.2f\n",avresec);
+			if(avrescs>avcs&&avresec>avec){counter++;}
+			x++;
+		}
+		printf("%d\n",counter);
+	}
+	return 0;
+}
