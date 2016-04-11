b667176eba47772bf3d117d046bc9ddde033be09
Yuwono Bangun Nagoro <morpheusgun@outlook.com>
2016-04-11 09:18:24 +0700

    Create Herman.cpp

---
+++ Kattis-Solutions/Herman.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define PI 3.141592653589793238462643383279502884197169399375105820974944592307816406286
+int main() {
+    double inp;
+    cin>>inp;
+    printf("%.6f\n",inp*inp*PI);
+    printf("%.6f\n",2*inp*inp);
+    return 0;
+}
