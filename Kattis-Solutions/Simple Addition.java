5dc2f4c3d649c1b1cf306ba5121ddc342b140d16
Yuwono Bangun Nagoro <morpheusgun@outlook.com>
2016-06-06 04:07:12 +0700

    Create Simple Addition.java

---
+++ Kattis-Solutions/Simple Addition.java
@@ -0,0 +1,21 @@
+    /* package whatever; // don't place package name! */
+     
+    import java.util.*;
+    import java.lang.*;
+    import java.io.*;
+    import java.math.*;
+    /* Name of the class has to be "Main" only if the class is public. */
+    class Ideone
+    {
+    	public static void main (String[] args) throws java.lang.Exception
+    	{
+    		Scanner sc=new Scanner(System.in);
+    		String s;
+    		s=sc.next();
+    		BigInteger a=new BigInteger(s);
+    		s=sc.next();
+    		BigInteger b=new BigInteger(s);
+    		b=b.add(a);
+    		System.out.println(b.toString());
+    	}
+    }
