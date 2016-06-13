df844a0cc703e5f62ba6e8891d71be7462f7645f
Yuwono Bangun Nagoro <morpheusgun@outlook.com>
2016-06-13 11:30:28 +0700

    Create My Simple CPP Template.cpp

---
+++ My Simple CPP Template.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define psb push_back
+#define mkp make_pair
+#define all(x) (x).begin(),(x).end()
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+int toInt(string x)
+{
+    istringstream ss(x);
+    int a;
+    ss>>a;
+    return a;
+}
+string tostr(int x)
+{
+    ostringstream ss;
+    ss<<x;
+    return ss.str();
+}
+int main() {
+	// your code goes here
+	
+	return 0;
+}
