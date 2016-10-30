2a77560d76b0a3f4c43cdf69cc63e694c8261518
Yuwono Bangun Nagoro <morpheusgun@outlook.com>
2016-10-30 12:20:29 +0700

    Create Quick Estimates.cpp

---
+++ Kattis-Solutions/Quick Estimates.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int tc;
+    string s;
+    scanf("%d",&tc);
+    for(int i=0;i<tc;i++)
+    {
+        cin>>s;
+        printf("%d\n",s.length());
+    }
+    return 0;
+}
