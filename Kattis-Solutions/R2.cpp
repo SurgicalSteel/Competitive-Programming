7d5687b086500fba720f0cc780e61a9002fca4c9
Yuwono Bangun Nagoro <morpheusgun@outlook.com>
2017-04-28 21:23:39 +0700

    Create R2.cpp

---
+++ Kattis-Solutions/R2.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int lli;
+typedef unsigned long long ull;
+#define psb push_back
+#define mkp make_pair
+#define all(x) (x).begin(),(x).end()
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+int toInt(string x)
+{
+    istringstream ss(x);
+    int a;
+    ss>>a;
+    return a;
+}
+string tostr(int x)
+{
+    ostringstream ss;
+    ss<<x;
+    return ss.str();
+}
+int main() {
+    // your code goes here
+    int a,b;
+    cin>>a>>b;
+    cout<<(b*2)-a<<"\n";
+    return 0;
+}
