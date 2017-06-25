0a2a9ea2a79ac6d2446c883bdde84f18718c3792
Yuwono Bangun Nagoro <morpheusgun@outlook.com>
2017-06-26 00:57:38 +0700

    Create Faktor.cpp

---
+++ Kattis-Solutions/Faktor.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int lli;
+typedef unsigned long long ull;
+#define psb push_back
+#define mkp make_pair
+#define all(x) (x).begin(),(x).end()
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+int main() {
+	// your code goes here
+	int na,ni;
+	cin>>na>>ni;
+	cout<<((ni-1)*na)+1<<"\n";
+	return 0;
+}
