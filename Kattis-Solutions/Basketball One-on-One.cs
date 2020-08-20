55d09de3f1837803b768071dd9e8f64a351c0805
Yuwono Bangun Nagoro <morpheusgun@outlook.com>
2020-08-20 10:41:41 +0700

    Create Basketball One-on-One.cs

---
+++ Kattis-Solutions/Basketball One-on-One.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Basketball_One_on_One
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            Console.WriteLine(s.Substring(s.Length - 2, 1));
+        }
+    }
+}
+
