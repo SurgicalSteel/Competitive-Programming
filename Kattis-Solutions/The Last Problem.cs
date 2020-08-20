79ba6d76e1363039a050f60f9709f07c29fd451b
Yuwono Bangun Nagoro <morpheusgun@outlook.com>
2020-08-20 10:42:17 +0700

    Create The Last Problem.cs

---
+++ Kattis-Solutions/The Last Problem.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace The_Last_Problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String s = Console.ReadLine();
+            Console.WriteLine("Thank you, {0}, and farewell!",s);
+        }
+    }
+}
