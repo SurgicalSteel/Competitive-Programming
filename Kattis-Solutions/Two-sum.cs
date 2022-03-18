c42e8cdcd0cbd427bc42ecb895704d8edaa10b0c
Yuwono Bangun Nagoro <morpheusgun@outlook.com>
2022-03-18 21:51:03 +0700

    Create Two-sum.cs

---
+++ Kattis-Solutions/Two-sum.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Two_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine().Split();
+            Console.WriteLine(int.Parse(arr[0]) + int.Parse(arr[1]));
+        }
+    }
+}
