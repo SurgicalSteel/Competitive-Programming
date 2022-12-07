cddc0da893925a74ee9a5bd8c82040d99f7eeb8a
Yuwono Bangun Nagoro <morpheusgun@outlook.com>
2022-12-07 13:39:25 +0700

    Create Knight Packing.cs

---
+++ Kattis-Solutions/Knight Packing.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KnightPacking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            if(n%2==1){Console.WriteLine("first");}else{Console.WriteLine("second");}
+        }
+    }
+}
