074b0852f18949fdd8d04f410157e5e1819ec370
Yuwono Bangun Nagoro <morpheusgun@outlook.com>
2023-04-08 23:48:06 +0700

    Create Arithmetic Functions.cpp

---
+++ Kattis-Solutions/Arithmetic Functions.cpp
@@ -0,0 +1,17 @@
+#include "arithmeticfunctions.h"
+
+// Compute x^3
+int cube(int x){
+    return x * x * x;
+}
+
+// Compute the maximum of x and y
+int max(int x, int y){
+    return x > y? x : y;
+}
+
+// Compute x - y
+int difference(int x, int y){
+    return x-y;
+}
+
