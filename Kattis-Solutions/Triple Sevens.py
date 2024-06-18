c6176c2d6ab002eca1ef6af3900475f687406458
Yuwono Bangun Nagoro <morpheusgun@outlook.com>
2024-06-19 04:29:22 +0700

    Create Triple Sevens.py

---
+++ Kattis-Solutions/Triple Sevens.py
@@ -0,0 +1,10 @@
+if __name__=="__main__":
+    sza = int(input())
+    a = [x for x in input().split() if x =="7"]
+    b = [x for x in input().split() if x =="7"]
+    c = [x for x in input().split() if x =="7"]
+
+    if len(a) >0 and len(b) >0 and len(c) >0:
+        print("777")
+    else:
+        print("0") 
