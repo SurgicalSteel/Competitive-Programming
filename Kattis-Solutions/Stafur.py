b12aa470d389f5a55e0b3c671d4dfb97b156ac52
Yuwono Bangun Nagoro <morpheusgun@outlook.com>
2024-07-01 03:54:19 +0700

    Create Stafur.py

---
+++ Kattis-Solutions/Stafur.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    vs = {"A","I","U","E","O"}
+    s = input()
+    if s in vs:
+        print("Jebb")
+    elif s == "Y":
+        print("Kannski")
+    else:
+        print("Neibb")
