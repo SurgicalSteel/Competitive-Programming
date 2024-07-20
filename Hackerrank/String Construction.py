5a33adbb3c166a3471badd409edb67c5d909104e
Yuwono Bangun Nagoro <morpheusgun@outlook.com>
2024-07-20 20:02:07 +0700

    Create String Construction.py

---
+++ Hackerrank/String Construction.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'stringConstruction' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def stringConstruction(s):
+    d = dict()
+    for x in range (len(s)):
+        if s[x] not in d:
+            d[s[x]] = 1
+    return len(d)
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = stringConstruction(s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
