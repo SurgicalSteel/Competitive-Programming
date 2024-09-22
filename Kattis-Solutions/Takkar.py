dd19af858c898ef2c1d0a872d4ae29ab50655fa2
Yuwono Bangun Nagoro <morpheusgun@outlook.com>
2024-09-22 21:07:11 +0700

    Create Takkar.py

---
+++ Kattis-Solutions/Takkar.py
@@ -0,0 +1,9 @@
+if __name__=="__main__":
+    a= int(input())
+    b= int(input())
+    if a>b:
+        print("MAGA!")
+    elif b>a:
+        print("FAKE NEWS!")
+    else:
+        print("WORLD WAR 3!")
